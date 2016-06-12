435cdb8d9aaaa6a3a9442f86d511fbd0f555d4a7
Jordan S. Gray <graybostephano@gmail.com>
2016-06-12 16:50:32 +0100

    Revert "Player movement added, game loop modified"
    This reverts commit 14c2d4939d579c423cbc7c4f6f5bc9e5f9625524.

--- SFML_Top_Down_Shooter/Game.cpp
+++ SFML_Top_Down_Shooter/Game.cpp
@@ -3,7 +3,7 @@
 
 //The constructor for a game object. Sets window and player attributes.
 Game::Game()
-	: mWindow(sf::VideoMode(1366, 768), "SFML GAME")
+	: mWindow(sf::VideoMode(640, 480), "SFML GAME")
 	, mPlayer()
 {
 	mPlayer.setRadius(40.0f);
@@ -14,29 +14,12 @@ Game::Game()
 //Main run function for the game object, calls several functions within game to carry out game loop.
 void Game::run()
 {
-	sf::Clock clock;
-
 	while (mWindow.isOpen())
 	{
-		sf::Time deltaTime = clock.restart();
 		processEvents();
-		update(deltaTime);
+		update();
 		render();
 	}
-}
-
-//Handles player input, such as the W A S D keys for movement.
-void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
-{
-	if (key == sf::Keyboard::W)
-		mIsMovingUp = isPressed;
-	else if (key == sf::Keyboard::A)
-		mIsMovingLeft = isPressed;
-	else if (key == sf::Keyboard::S)
-		mIsMovingDown = isPressed;
-	else if (key == sf::Keyboard::D)
-		mIsMovingRight = isPressed;
-
 }
 
 /*
@@ -51,36 +34,18 @@ void Game::processEvents()
 	//If the close event is selected whilst the window is open, close the window.
 	while (mWindow.pollEvent(event))
 	{
-		switch (event.type)
+		if (event.type == sf::Event::Closed)
 		{
-		case sf::Event::KeyPressed:
-			handlePlayerInput(event.key.code, true);
-			break;
-		case sf::Event::KeyReleased:
-			handlePlayerInput(event.key.code, false);
-			break;
-		case sf::Event::Closed:
 			mWindow.close();
-			break;
 		}
 	}
 
 }
 
 //Updates the game logic - everything that happens in the game.
-void Game::update(sf::Time deltaTime)
+void Game::update()
 {
-	sf::Vector2f movement(0.0f, 0.0f);
-	if (mIsMovingUp == true)
-		movement.y -= playerSpeed;
-	if (mIsMovingDown == true)
-		movement.y += playerSpeed;
-	if (mIsMovingLeft == true)
-		movement.x -= playerSpeed;
-	if (mIsMovingRight == true)
-		movement.x += playerSpeed;
 
-	mPlayer.move(movement * deltaTime.asSeconds());
 }
 
 /*
--- SFML_Top_Down_Shooter/Game.h
+++ SFML_Top_Down_Shooter/Game.h
@@ -6,20 +6,12 @@ class Game
 public:
 	Game();
 	void run();
-	void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 	void processEvents();
-	void update(sf::Time deltaTime);
+	void update();
 	void render();
 	
 private:
 	sf::RenderWindow mWindow;
 	sf::CircleShape mPlayer;
-
-	bool mIsMovingUp;
-	bool mIsMovingDown;
-	bool mIsMovingLeft;
-	bool mIsMovingRight;
-
-	float playerSpeed = 100.0f;
 };
 
